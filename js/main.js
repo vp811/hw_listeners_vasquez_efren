9921d69b4ce1d4c24b40ce2028a44b9d58b79059
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-23 17:04:38 -0400

    create JS code for listener homework problem

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('listener').addEventListener('click', myFunction);
+
+var i=1;
+function myFunction(){
+
+    var paragraphTag = document.createElement("p");
+    var sentence = document.createTextNode("This is click number " + i);
+
+
+    paragraphTag.appendChild(sentence);
+    var element = document.getElementById("listener");
+    element.appendChild(paragraphTag);
+
+i++;
+}
