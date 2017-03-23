16636639a5858904f63a7c1f41a61a33e34dfff7
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-23 17:10:00 -0400

    edit errors on javascript file

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 document.getElementById('listener').addEventListener('click', myFunction);
 
-var i=1;
-function myFunction(){
+var i = 1;
+function myFunction() {
 
-    var paragraphTag = document.createElement("p");
-    var sentence = document.createTextNode("This is click number " + i);
+    var paragraphTag = document.createElement('p');
+    var sentence = document.createTextNode('This is click number ' + i);
 
 
     paragraphTag.appendChild(sentence);
-    var element = document.getElementById("listener");
+    var element = document.getElementById('listener');
     element.appendChild(paragraphTag);
 
-i++;
+    i++;
 }
