e704b390b51d2ddab13cb27e364619ee802ad283
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-23 23:56:56 -0400

    create javascript code for form branch

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener( 'submit', function(event) {
+    event.preventDefault();
+
+
+var firstName = document.getElementById( 'firstname' ).value;
+var lastName = document.getElementById( 'lastname' ).value;
+var email = document.getElementById ( 'email' ).value;
+var message = document.getElementById ( 'message' ).value;
+
+
+    console.log( "The First Name is: " + firstName );
+    console.log( "The Last Name is: " + lastName );
+    console.log( "The Email is: " + email );
+    console.log( "The message is: " + message );
+
+    console.log( "I think your site is quite beautiful. Also, this form is great!" );
+})
