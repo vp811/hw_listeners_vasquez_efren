5dcd344f012a0c980c1f12c24eb9279e39544336
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-24 01:09:47 -0400

    add comments to each event listener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/*---------------------Element One--------------*/
+/*---------------------Element One---------------*/
+/*---------------------Listens for mouse click---------------*/
+/*---------------------Changes background Color---------------*/
 
 var x = document.getElementById('elementOne');
 x.addEventListener('click', backgroundColor);
@@ -8,14 +10,20 @@ function backgroundColor() {
 }
 
 /*---------------------Element Two--------------*/
+/*---------------------Listens for mouse hover---------------*/
+/*---------------------Changes background Color, font color, and font size---------------*/
 var y = document.getElementById('elementTwo');
 y.addEventListener('mouseover', font);
 
 function font() {
     y.style.color = 'yellow';
+    y.style.backgroundColor= 'blue';
+    y.style.fontSize= '1.3em';
 }
 
 /*---------------------Element Three--------------*/
+/*---------------------Listens for double click---------------*/
+/*---------------------Changes the text align to right---------------*/
 var z = document.getElementById('elementThree');
 z.addEventListener('dblclick', padding);
 
